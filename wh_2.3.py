c1a239466fa5c80488b4baee4279b52ccd09b470
Roman <34662053+ParshutinRoman@users.noreply.github.com>
2018-04-17 12:38:00 +0300

    Add files via upload

--- wh_2.3.py
+++ wh_2.3.py
@@ -1,8 +1,10 @@
 import chardet
 import json
 from collections import Counter
+from pprint import pprint
 
 files = ('newsafr.json', 'newscy.json', 'newsfr.json', 'newsit.json')
+
 
 
 for file in files:
@@ -11,14 +13,16 @@ for file in files:
         result = chardet.detect(data)
         data = bytes.decode(data, encoding=result['encoding'])
         data = json.loads(data)
-        text = data['rss']['channel']['items'][1]['description']
-        words = text.split(' ')
-        words_short = []
-        for word in words:
-            if len(word) > 6:
-                words_short.append(word)
-        words_short.sort()
-        count = Counter(words_short)
-        print('топ 10 самых часто встречающихся слов длиннее 6 символов:', count.most_common(10))
+        text = data['rss']['channel']['items']
+        for news in text:
+            words = news['description']
+            words = words.split(' ')
+            words_short = []
+            for word in words:
+                if len(word) > 6:
+                    words_short.append(word)
+            count = Counter(words_short)
+            print('топ 10 самых часто встречающихся слов длиннее 6 символов:', count.most_common(10))
+
 
 
